d22116b927cc892863d04a13ea5f15d058cf1c37
Siarhei Semikalenau <Siarhei_Semikalenau@epam.com>
2018-06-22 15:41:00 +0300

    Added baseline

--- bin/perf_add_comment.sh
+++ bin/perf_add_comment.sh
@@ -1,43 +1,76 @@
 #!/usr/bin/env bash
 
-URL=http://192.168.99.100:8080
+HOST=http://192.168.99.100:8080
+
+SCRIPTPATH=$( cd "$(dirname "$0")" ; pwd -P )
+
+DT=`date +%Y%m%d-%H%M%S`
+
+usage () {
+	echo "Usage: $0 1|5 [base|compare]"
+	echo "	1|5 - number of virtual users"
+	echo "	base - generate baseline file"
+	echo "	compare - comparing current test with baseline and output results"
+	echo "		1st column - request name"
+	echo "		2nd column - baseline response time (in ms)"
+	echo "		3rd column - test response time (in ms)"
+	echo "		4th column - difference (in ms). Negative better than baseline, positive - worse"
+	exit 0
+}
 
 if [[ "$#" -lt 1 ]]; then 
-  echo $"Usage: $0 1|5"
-  exit 1
+  usage
 fi
 
 NUMCL=$1
 case $NUMCL in
 	1)
-	CLIENTS=1
-	RUNTIME=20s
-	;;
+		CLIENTS=1
+		RUNTIME=20s
+		shift
+		;;
 
 	5)
-	CLIENTS=5
-	RUNTIME=120s
-	;;
+		CLIENTS=5
+		RUNTIME=120s
+		shift
+		;;
 
 	*)
-	echo $"Usage: $0 1|5"
- 	exit 1
-	;;
+ 		usage
+		;;
 
 esac
 
-SCRIPTPATH=$( cd "$(dirname "$0")" ; pwd -P )
+OP=$1
+case "$OP" in
+	base)
+		CSVF=${SCRIPTPATH}/perf_add_comment_${CLIENTS}_base
+		LOGF=${SCRIPTPATH}/perf_add_comment_${CLIENTS}_base.log
+		;;
 
-DT=`date +%Y%m%d-%H%M%S`
-LOGF=${SCRIPTPATH}/perf_add_comment_${DT}.log
-CSVF=${SCRIPTPATH}/perf_add_comment_${DT}
+	compare|"")
+		CSVF=${SCRIPTPATH}/perf_add_comment_${CLIENTS}_${DT}
+		BASEF=${SCRIPTPATH}/perf_add_comment_${CLIENTS}_base
+		LOGF=${SCRIPTPATH}/perf_add_comment_${CLIENTS}_${DT}.log
+		;;
 
-locust --locustfile=${SCRIPTPATH}/locust-test.py --host=${URL} --no-web --clients=${CLIENTS} --hatch-rate=1 --run-time=${RUNTIME} --loglevel=ERROR --logfile=${LOGF} --only-summary --csv=${CSVF}
+	*)
+		usage
+		;;
+esac
+
+locust --locustfile=${SCRIPTPATH}/locust-test.py --host=${HOST} --no-web --clients=${CLIENTS} --hatch-rate=1 --run-time=${RUNTIME} --loglevel=ERROR --logfile=${LOGF} --only-summary --csv=${CSVF}
 ret=$?
 
 if [ $ret -ne 0 ]; then
 	echo Fail
 	exit 2
+fi
+
+if [[ "$OP" == "compare" ]]; then
+	paste -d, <(cut -d, -f2,6 <(tail -n +2 <(head -n -1 ${BASEF}_requests.csv))) <(cut -d, -f6 <(tail -n +2 <(head -n -1 ${CSVF}_requests.csv))) | awk -F, '{OFS=","; print $1,$2,$3,$3-$2}' > ${CSVF}_compare.csv
+	cat ${CSVF}_compare.csv
 fi
 
 f=$(grep '040\.' ${CSVF}_requests.csv | cut -d, -f6)
@@ -50,4 +83,3 @@ else
 	echo Fail
 	exit 1
 fi
-
